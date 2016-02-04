ae178885d082e4c496f0ae6019c2b0408c901adf
hhsadiq <hhsadiq@gmail.com>
2016-02-04 08:55:16 +0500

    added a form component

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
+import {FormComponent} from './form.component'
 
 //noinspection TypeScriptValidateTypes
 bootstrap(AppComponent);
