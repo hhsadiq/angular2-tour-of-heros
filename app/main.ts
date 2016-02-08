f07e921d7245b2f509949423db724453ace82942
hhsadiq <hhsadiq@gmail.com>
2016-02-08 08:44:05 +0500

    master detail separate components

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
-import {FormComponent} from './form.component'
 
 //noinspection TypeScriptValidateTypes
 bootstrap(AppComponent);
